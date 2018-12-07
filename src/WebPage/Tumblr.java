1e60377756f464b6c824965dab2cfb68190cb7ee
iluvjava <victor1301166040@gmail.com>
2018-12-06 16:48:43 -0800

    N/a

---
+++ src/WebPage/Tumblr.java
@@ -0,0 +1,35 @@
+package WebPage;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import Scraping.DownLoader;
+import Scraping.Scrapable;
+
+public class Tumblr implements Scrapable{
+
+	@Override
+	public Scrapable prepare() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Scrapable> getNextWebPages() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doTheScraping(DownLoader dl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSourceContentUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
